5eccd730df063726a712f010458c85bf125fbb74
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2015-07-30 16:06:07 -0500

    change version

--- Service/FiftyOneDegreesWrapper.php
+++ Service/FiftyOneDegreesWrapper.php
@@ -8,7 +8,7 @@ class FiftyOneDegreesWrapper
     {
         // Configure 51Degrees operation
         global $_fiftyone_degrees_data_file_path;
-        $_fiftyone_degrees_data_file_path = __DIR__.'/../Library/51Degrees-PremiumV3_1.dat';
+        $_fiftyone_degrees_data_file_path = __DIR__.'/../Library/51Degrees-PremiumV3_2.dat';
 
         // class
         require_once(__DIR__.'/../Library/51Degrees_data_reader.php');

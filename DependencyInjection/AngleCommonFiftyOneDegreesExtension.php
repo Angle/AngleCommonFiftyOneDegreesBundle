cb5c51575ec1803cd2dc7d9d945f168f23ed5aab
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2015-07-30 17:15:57 -0500

    hope it works

--- DependencyInjection/AngleCommonFiftyOneDegreesExtension.php
+++ DependencyInjection/AngleCommonFiftyOneDegreesExtension.php
@@ -2,23 +2,22 @@
 namespace Angle\Common\FiftyOneDegreesBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
 
 
 class AngleCommonFiftyOneDegreesExtension extends Extension
 {
     /**
-     * @param array            $configs
-     * @param ContainerBuilder $container
+     * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
         if (!isset($config['data_file_path'])) {

e9ea33623826fe7377de2a21481d188fa6a62ec1
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2015-07-30 12:44:25 -0500

    try store en library when run update

--- Service/FiftyOneDegreesWrapper.php
+++ Service/FiftyOneDegreesWrapper.php
@@ -4,11 +4,11 @@ namespace Angle\Common\FiftyOneDegreesBundle\Service;
 
 class FiftyOneDegreesWrapper
 {
-    public function __construct($data_file_path=null)
+    public function __construct()
     {
         // Configure 51Degrees operation
         global $_fiftyone_degrees_data_file_path;
-        $_fiftyone_degrees_data_file_path = $data_file_path.'/51Degrees-PremiumV3_1.dat';
+        $_fiftyone_degrees_data_file_path = __DIR__.'/../Library/51Degrees-PremiumV3_1.dat';
 
         // class
         require_once(__DIR__.'/../Library/51Degrees_data_reader.php');
